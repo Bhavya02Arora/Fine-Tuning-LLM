740666de46db7f603ee7cfde9d8c32567db9c346
Bhavya Arora <bhavya789arora@gmail.com>
2025-06-18 18:23:53 +0530

    Removed hard coded values and modified README.md file

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ print(response)
 # FineTuningJob(id='ftjob-9dD7SotEdES0xy7ac3dbcHSv', created_at=1750245639, error=Error(code=None, message=None, param=None), fine_tuned_model=None, finished_at=None, hyperparameters=Hyperparameters(batch_size='auto', learning_rate_multiplier='auto', n_epochs='auto'), model='gpt-3.5-turbo-0125', object='fine_tuning.job', organization_id='org-bZ9KN2rdozJ7snHuVnypRDGM', result_files=[], seed=1473300658, status='validating_files', trained_tokens=None, training_file='file-FDxAUGtaVmTLHKuPNa8ZwQ', validation_file=None, estimated_finish=None, integrations=[], metadata=None, method=Method(type='supervised', dpo=None, reinforcement=None, supervised=SupervisedMethod(hyperparameters=SupervisedHyperparameters(batch_size='auto', learning_rate_multiplier='auto', n_epochs='auto'))), user_provided_suffix=None, usage_metrics=None, shared_with_openai=False, eval_id=None)
 
 print(client.fine_tuning.jobs.list(limit=10))
-
-print(client.fine_tuning.jobs.retrieve("ftjob-2TJ5ZZX1WPvithAFu9zrSPUT"))
+# to retrieve a specific fine-tuning job by its ID
+# print(client.fine_tuning.jobs.retrieve("fine-tuning-job-id"))
 
 response = client.chat.completions.create(
     model="ft:gpt-3.5-turbo-0125:personal::BjlIyXNp",
